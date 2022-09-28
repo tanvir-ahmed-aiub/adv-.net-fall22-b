b9ec31abfa76e0200506e55432787aea7a6d5999
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-09-28 09:31:22 +0600

    Form validation

---
+++ Intro/Intro/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Course c) {
+            if (ModelState.IsValid) {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(c);
+        }
+        //public ActionResult Submit(Course c) {
+        //    // ViewBag.Name = Request["Name"];
+        //    //ViewBag.Name = form["Name"];
+        //    //ViewBag.Name = c.Name;
+        //    return View(c);
+        //}
+    }
+}
---
+++ Intro/Intro/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Intro.Models
+{
+    public class Course
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(25,ErrorMessage ="Name should not exceeed 25 chars")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Provide name of teacher")]
+        [StringLength(25)]
+        public string Teacher { get; set; }
+    }
+}
